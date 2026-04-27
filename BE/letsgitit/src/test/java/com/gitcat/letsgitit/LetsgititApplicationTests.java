c63be109b8b29e61d18c91326f844128b41dfa64
pyy2114 <pyy2114@naver.com>
2026-04-27 16:21:08 +0900

    [BE] chore: 백엔드 초기 코드 셋팅

---
+++ BE/letsgitit/src/test/java/com/gitcat/letsgitit/LetsgititApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitcat.letsgitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsgititApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
