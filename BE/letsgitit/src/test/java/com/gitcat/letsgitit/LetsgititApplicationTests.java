42f676d9f29f5fd7ed132c63d7c2e4059656cb64
조수진 <wls7911@naver.com>
2026-04-30 17:03:39 +0900

    [공통/fix/151] be-ci 오류 수정

--- BE/letsgitit/src/test/java/com/gitcat/letsgitit/LetsgititApplicationTests.java
+++ BE/letsgitit/src/test/java/com/gitcat/letsgitit/LetsgititApplicationTests.java
@@ -1,10 +1,17 @@
 package com.gitcat.letsgitit;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LetsgititApplicationTests {
+
+	@MockitoBean
+	RedissonClient redissonClient;
 
 	@Test
 	void contextLoads() {}
