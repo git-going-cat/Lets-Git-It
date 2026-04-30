f186ead8b2adc60821d61f82c0da206ebffc5b78
김효선 <ssunykkim@naver.com>
2026-04-30 17:44:15 +0900

    [BE] chore: 불필요한 파일 삭제

--- BE/letsgitit/src/main/java/com/gitcat/letsgitit/domain/auth/exception/AuthException.java
+++
@@ -1,3 +0,0 @@
-package com.gitcat.letsgitit.domain.auth.exception;
-
-public class AuthException {}
--- BE/letsgitit/src/main/java/com/gitcat/letsgitit/domain/room/exception/RoomException.java
+++
@@ -1,3 +0,0 @@
-package com.gitcat.letsgitit.domain.room.exception;
-
-public class RoomException {}
