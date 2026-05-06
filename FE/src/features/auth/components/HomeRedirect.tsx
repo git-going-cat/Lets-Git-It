77a9125942fcda26b0ab05a3251ec6389052e223
pengom0809 <pengom0809@gmail.com>
2026-05-06 13:57:37 +0900

    [FE] fix: 온보딩 중간에 이탈자가 발생했을 때 브라우저를 다시 열면 홈으로 바로 들어올 수 있는 버그 수정

--- FE/src/features/auth/components/HomeRedirect.tsx
+++ FE/src/features/auth/components/HomeRedirect.tsx
@@ -7,9 +7,14 @@ import ReactivationNotice from './ReactivationNotice';
 export default function HomeRedirect() {
   const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
   const pendingReactivationNotice = useAuthStore((s) => s.pendingReactivationNotice);
+  const onboardingStatus = useAuthStore((s) => s.user?.onboardingStatus);
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
+  }
+
+  if (onboardingStatus !== 'TUTORIAL_DONE') {
+    return <Navigate to="/onboarding" />;
   }
 
   if (pendingReactivationNotice) {
