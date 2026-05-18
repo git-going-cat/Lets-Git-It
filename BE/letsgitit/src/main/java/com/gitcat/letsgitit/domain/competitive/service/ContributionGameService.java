60733f172e2c712e29b45a2fdbbeff7df630b9e3
pyy2114 <pyy2114@naver.com>
2026-05-18 14:36:48 +0900

    [BE] style: 포맷터 적용

--- BE/letsgitit/src/main/java/com/gitcat/letsgitit/domain/competitive/service/ContributionGameService.java
+++ BE/letsgitit/src/main/java/com/gitcat/letsgitit/domain/competitive/service/ContributionGameService.java
@@ -6,8 +6,8 @@ import java.util.UUID;
 import com.gitcat.letsgitit.domain.competitive.dto.ContributionInputResult;
 import com.gitcat.letsgitit.domain.competitive.dto.ContributionSessionCommand;
 import com.gitcat.letsgitit.domain.competitive.dto.ContributionSessionPlayer;
-import com.gitcat.letsgitit.domain.competitive.message.contribution.ContributionGameEndMessage;
 import com.gitcat.letsgitit.domain.competitive.message.contribution.ContributionExpireRequestMessage;
+import com.gitcat.letsgitit.domain.competitive.message.contribution.ContributionGameEndMessage;
 import com.gitcat.letsgitit.domain.competitive.message.contribution.ContributionInputMessage;
 
 public interface ContributionGameService {
